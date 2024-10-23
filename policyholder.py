4ca05d95022571b28625aabd14027e32ece4a103
MugoDom <njuemugodominic@gmail.com>
2024-10-23 10:20:42 +0300

    define register, suspend and reactivate methods for policyholder class

--- policyholder.py
+++ policyholder.py
@@ -1,4 +1,19 @@
 # Define a policy holder class
-class policyholder:
-    def __init__(self, policyholder_id, name, status='Active') -> None:
+class Policyholder:
+    def __init__(self, policyholder_id, fname, lname, address, status='Active'):
+        self.policyholder_id = policyholder_id
+        self.fname = fname
+        self.lname = lname
+        self.email = fname + '.' + lname + '@email.com'
+        self.address = address
+        self.status = status
+
+    def register(self):
         pass
+
+    def suspend(self):
+        pass
+
+    def reactivate(self):
+        pass
+
